65a61af61ee68dc48f36fc09574ce3914ea39358
Karolis Kučinskas <karolis.kucinskas@mif.stud.vu.lt>
2021-05-08 18:55:01 +0300

    v1.2

---
+++ student.cpp
@@ -0,0 +1,51 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
+#include "stdlib.h"
+#include <fstream>
+#include <sstream>
+#include <list>
+#include <deque>
+
+#include "header.h"
+
+using std::string;
+
+student::student() { }
+
+student::student(const student &s) {
+    vardas = s.vardas;
+    pavarde = s.pavarde;
+    vid = s.vid;
+}
+
+student::~student() {
+
+}
+
+
+string student::getVardas() const {
+    return vardas;
+}
+
+string student::getPavarde() const {
+    return pavarde;
+}
+
+double student::getVid() const {
+    return vid;
+}
+
+void student::setVardas(string vardas) {
+    this->vardas = vardas;
+}
+
+void student::setPavarde(string pavarde) {
+    this->pavarde = pavarde;
+}
+
+void student::setVid(double vid) {
+    this->vid = vid;
+}
